72e2d1c1cef9de725ee8b32282d9b5c6daf332e0
KyleBCox <kylecoxforweb@gmail.com>
2020-01-02 12:23:39 +0000

    reshuffle

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-nasm -f elf boot.asm -o boot.o
-g++ -m32 -c  kentry.c -ffreestanding -fno-exceptions -fno-rtti
-ld -m elf_i386 -T linker.ld kentry.o boot.o -o os1 -nostdlib
+nasm -f elf ./src/boot.asm -o boot.o
+g++ -m32 -c  ./src/kentry.c -ffreestanding -fno-exceptions -fno-rtti
+ld -m elf_i386 -T ./src/linker.ld kentry.o boot.o -o os1 -nostdlib
 
 
-grub-file --is-x86-multiboot os1.bin
+grub-file --is-x86-multiboot os1
 
-mkdir -p isodir/boot/grub
-cp os1 isodir/boot/os1
-cp grub.cfg isodir/boot/grub/grub.cfg
-grub-mkrescue -o os1.iso isodir
+mkdir -p iso/boot/grub
+cp os1 iso/boot/os1
+cp ./src/grub.cfg iso/boot/grub/grub.cfg
+grub-mkrescue -o os1.iso iso
